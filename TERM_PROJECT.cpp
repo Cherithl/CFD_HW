f9b8527f3ce5f85fee369bcf89dfdb82bfb7d596
Cherithl <cherithl@vt.edu>
2023-04-27 21:19:27 -0400

    new commit

--- TERM_PROJECT.cpp
+++ TERM_PROJECT.cpp
@@ -14,6 +14,7 @@ typedef vector<vector<double>> matrix;
 void FV_Momentum();
 void Pressure_Poisson();
 void Velocity_Correction();
+void Velocity_L2Norm();
 
 //FUNCTIONS FOR OPERATIONS
 std::tuple<matrix,matrix> Update_Boundaries( matrix , matrix );
@@ -21,21 +22,23 @@ void printmatrix( matrix );
 
 //DOMAIN PARAMETERS
 const double lx=1 , ly =1;
-int nx =6 , ny =6;
+int nx = 10 , ny =nx;
 int n = (nx+2)*(ny+2);
 const double dx = lx/nx , dy = ly/ny;
 
 //PHYSICAL PARAMETERS
 double Re=100 ;
 
-
+/*SOLVER PARAMETERS*/
 int TIME_ITER=0;
+double err_u =1 ; double err_v=1 ;
+
 
 //TIME STEP
-double dt = 0.02;
+double dt = 5.0E-4;
 
 
-//VELOCITY AND PRESSURE MATRICES
+/*VELOCITY MATRICES FOR MOMENTUM EQUATIONS */
 matrix u_nm1(ny+2,vector<double>(nx+2,0.0));
 matrix v_nm1(ny+2,vector<double>(nx+2,0.0));
 matrix u_n(ny+2,vector<double>(nx+2,0.0));
@@ -43,50 +46,44 @@ matrix v_n(ny+2,vector<double>(nx+2,0.0));
 matrix u_tilda(ny+2,vector<double>(nx+2,0.0));
 matrix v_tilda(ny+2,vector<double>(nx+2,0.0));
 
+/*MATRICES DECLARATION FOR POISSON EQUATION SOLVER*/
 matrix P_corr(ny+2,vector<double>(nx+2,0.0));
+matrix Ae(ny+2,vector<double>(nx+2,1.0/(pow(dx,2))));
+matrix Aw(ny+2,vector<double>(nx+2,1.0/(pow(dx,2))));
+matrix An(ny+2,vector<double>(nx+2,1.0/(pow(dy,2))));
+matrix As(ny+2,vector<double>(nx+2,1.0/(pow(dy,2))));
 
 
 
 
 int main(){
-//ENFORCING BOUNDARY CONDITIONS    
-    for( int i=1 ; i<ny+1 ; ++i ){
-        u_nm1[i][1] = 0;    v_nm1[i][0]   = 2*0 -  v_nm1[i][1];   //LEFT BOUNDARY 
-        u_nm1[i][nx+1]= 0;  v_nm1[i][nx+1]= 2*0 - v_nm1[i][nx];  //RIGHT BOUNDARY
-    }
-    for(int j=1 ; j<nx+1 ; ++j){
-        u_nm1[ny+1][j] = 2*1.0 - u_nm1[ny][j] ;  v_nm1[ny+1][j] = 0; //TOP WALL
-        u_nm1[0][j]    = 2*0 - u_nm1[1][j];      v_nm1[0][j]    = 0; //BOTTOM WALL
-    }
-    u_n = u_nm1; v_n = v_nm1; //for the time being so that we get EXPLCIT TIME INTEGRATION in the first time integration step
+/*ENFORCING BOUNDARY CONDITIONS*/  
+    std::tie( u_nm1 , v_nm1 )   =  Update_Boundaries( u_nm1 , v_nm1 );
+    std::tie( u_n , v_n )       =  Update_Boundaries( u_n , v_n );
 
-double err_u =1 ; double err_v=1 ;
-double vel_tol = -8;
-
-    while( log10(err_u) > vel_tol && log10(err_v) > vel_tol && TIME_ITER<2){
-        cout<<" I AM HERE MF "<<endl<<endl<<endl;
+//FRACTIONAL STEP ALGORITHM STARTS HERE ---------------------------------------------------------------------------------------------------------------
+    
+    double vel_tol = -8;
+while( (log10(err_u) > vel_tol || log10(err_v) > vel_tol ) || TIME_ITER<100){
         err_u=0; err_v=0;
-        FV_Momentum();
-        Pressure_Poisson();
-        Velocity_Correction();
-        for( int i=1 ; i<ny+1 ; ++i){
-            for( int j=3 ; j<nx+1 ; ++j){
-                err_u = pow( u_n[i][j] - u_nm1[i][j] , 2 )/(nx*ny);
-            }
-        }
-        for( int i=2 ; i<ny+1 ; ++i){
-            for( int j=1 ; j<nx+1 ; ++j){
-                err_v = pow( v_n[i][j] - v_nm1[i][j] , 2 )/(nx*ny);
-            }
-        }
-
-    err_u = pow( err_u , 0.5 ); err_v = pow( err_v , 0.5 );
-    TIME_ITER++;
-    }
-
+    FV_Momentum();
+    Pressure_Poisson();
+    Velocity_Correction();
+    Velocity_L2Norm();
+ 
+TIME_ITER++;
+cout<<"TIME ITER :  "<<TIME_ITER<<endl;
+cout<<" ERR U : "<<err_u<<endl;  cout<<" ERR V : "<<err_v<<endl;
+}
+printmatrix(P_corr);cout<<endl<<endl;
+printmatrix(u_n);cout<<endl<<endl;
+cout<<" ERR U : "<<err_u<<endl;  cout<<" ERR V : "<<err_v<<endl;
 }
 
 
+
+
+/*---------------------------------------------FUNCTION TO UPDATE THE BOUNDARY VALUES FROM EXISITING BOUNDARY CONDITIONS-------------------------------------------------*/
 std::tuple<matrix,matrix> Update_Boundaries( matrix u , matrix v ){
        for( int i=1 ; i<ny+1 ; ++i ){
         u[i][1] = 0;    v[i][0]   = 2*0 -  v[i][1];   //LEFT BOUNDARY 
@@ -99,9 +96,10 @@ std::tuple<matrix,matrix> Update_Boundaries( matrix u , matrix v ){
     return std::make_tuple( u , v  ) ; 
 }
 
+
 void FV_Momentum(){
 //X-MOMENTUM TIME INTEGRATION
-printmatrix(u_n);cout<<endl<<endl;
+// printmatrix(u_n);cout<<endl<<endl;
         for( int i=1 ; i<ny+1 ; ++i){
             for( int j=2 ; j<nx+1 ; ++j){
                 double ue_nm1 = 0.5*( u_nm1[i][j] + u_nm1[i][j+1] ),     ue_n = 0.5*( u_n[i][j] + u_n[i][j+1] );
@@ -121,7 +119,8 @@ printmatrix(u_n);cout<<endl<<endl;
 
             }
         }
-printmatrix(u_tilda);cout<<endl<<endl;
+        /*CONSIDER UPDATING BOUNDARIES I DONNO IF IT IMRPOVES OR DEGRADES*/
+
 //Y-MOMENTUM TIME INTEGRATION
         for( int i=2 ; i<ny+1 ; ++i){
             for( int j=1 ; j<nx+1 ; ++j){
@@ -143,48 +142,67 @@ printmatrix(u_tilda);cout<<endl<<endl;
             }
         }
     u_nm1 = u_n; v_nm1 = v_n; //SWAPPING (n-1) st step
-    
+    std::tie( u_tilda , v_tilda )       =  Update_Boundaries( u_tilda , v_tilda );
+ //printmatrix(u_tilda);cout<<endl<<endl;
 }
 
 
 void Pressure_Poisson(){
-//BOUNDARY CONDITIONS 
-matrix S(ny+2,vector<double>(nx+2,0.0));
-matrix res( ny+2,vector<double>(nx+2,0.0));
-    for( int i=1 ; i<ny+1 ; ++i){
+
+/* - - - - - - - - - - - - - - - - - - - -  - - - - - - - - -  - - - - For Pressure Boundary Conditions - - - - - - - - - - - - - - - - - - - -  - - - - - - - - -  - - - - */
+    if( TIME_ITER==1 ){
+        for( int i=1 ; i<ny+1 ; ++i){
+            Ae[i][nx] =0.0; 
+            Aw[i][1]  =0.0;
+        }
         for( int j=1 ; j<nx+1 ; ++j){
-            S[i][j] =   (1.0/dt)*( ( u_tilda[i][j+1] - u_tilda[i][j] )/dx + ( v_tilda[i+1][j] - v_tilda[i][j] )/dy );
+            An[ny][j] = 0.0;
+            As[1][j]  = 0.0;
+        }    
+    }
+
+
+/*- - - - - - - - - - - - - - - - - - - -  - - - - - - - - -  - - - - INITILISE THE PRESSURE CORRECTION MATRIX- - - - - - - - - - - - - - - - - - - -  - - - - - - - - -  - - - - */
+    for( int i=0 ; i<ny+2 ; ++i){
+        for( int j=0 ; j<nx+2 ; ++j){
+            P_corr[i][j] = 0.0;
         }
     }
-double AE = 1.0/pow(dx,2), AW =AE;
-double AN = 1.0/pow(dy,2), AS =AN;
-double AP = AE+AW+AN+AS;
-double tol = -5; double res_avg =1;
-        while ( log10(res_avg) > tol ){
-            for( int i=1 ; i<ny+1 ; ++i ){
-                P_corr[i][0] = P_corr[i][1];
-                P_corr[i][nx+1] = P_corr[i][nx];
-            }    
 
+
+    /*- - - - - - - - - - - - - - - - - - - -  - - - - - - - - -  - - - - SOURCE TERMS FOR THE PRESSURE POISSION EQUATION- - - - - - - - - - - - - - - - - - - -  - - - - - - - - -  - - - - */
+    matrix S(ny+2,vector<double>(nx+2,0.0));
+    matrix res( ny+2,vector<double>(nx+2,0.0));
+        for( int i=1 ; i<ny+1 ; ++i){
             for( int j=1 ; j<nx+1 ; ++j){
-                P_corr[0][j] = P_corr[1][j];
-                P_corr[nx+1][j] = P_corr[nx][j];
+                S[i][j] =   (1.0/dt)*( ( u_tilda[i][j+1] - u_tilda[i][j] )/dx + ( v_tilda[i+1][j] - v_tilda[i][j] )/dy );
             }
-        //GAUSS SEIDEL 
+        }
+
+
+    double tol = -5; double res_avg =1;
+        while ( log10(res_avg) > tol ){
+        
                 for( int i=1 ; i<ny+1 ; ++i){
                     for( int j=1; j<nx+1 ; ++j){
+                        double AE = Ae[i][j] , AW = Aw[i][j] , AN = An[i][j] , AS = As[i][j] , AP = AE + AW + AN + AS ;
+
                         P_corr[i][j] = (1.0/AP)*( AW*P_corr[i][j-1] + AE*P_corr[i][j+1] + AN*P_corr[i+1][j] + AS*P_corr[i-1][j] - S[i][j]  ) ;
+                    
                     }
                 }
             res_avg=0;      
                 for( int i=1; i<ny+1 ; ++i){
                     for( int j=1 ; j<nx+1 ; ++j){
+                        double AE = Ae[i][j] , AW = Aw[i][j] , AN = An[i][j] , AS = As[i][j] , AP = AE + AW + AN + AS ;
+
                         res[i][j] = AP*P_corr[i][j] - ( AW*P_corr[i][j-1] + AE*P_corr[i][j+1] + AN*P_corr[i+1][j] + AS*P_corr[i-1][j] - S[i][j]  );
+                        
                         res_avg += pow( res[i][j] ,2)/(nx*ny);
+
                     }
                 }
             res_avg = pow(res_avg,0.5);
-        //cout<<res_avg<<endl;
         }
 
 }
@@ -201,8 +219,26 @@ void Velocity_Correction(){
             v_n[i][j] = v_tilda[i][j] - dt*( P_corr[i][j] - P_corr[i-1][j] )/dy;
         }
     }
+    std::tie( u_n , v_n ) = Update_Boundaries( u_n , v_n );
 
 }
+
+
+
+void Velocity_L2Norm(){
+    for( int i=1 ; i<ny+1 ; ++i){
+        for( int j=2 ; j<nx+1 ; ++j){
+            err_u = pow( u_n[i][j] - u_nm1[i][j] , 2 )/(nx*ny);
+        }
+    }
+    for( int i=2 ; i<ny+1 ; ++i){
+        for( int j=1 ; j<nx+1 ; ++j){
+                err_v = pow( v_n[i][j] - v_nm1[i][j] , 2 )/(nx*ny);
+        }
+    }
+    err_u = pow( err_u , 0.5 ); err_v = pow( err_v , 0.5 );
+}
+
 
 
 void printmatrix( matrix mat){
